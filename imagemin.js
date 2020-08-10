4bd8cd31145f58edbfe6cc8978aaf89bf51251bc
Nhat Do <ddnhat@gmail.com>
2020-08-10 10:11:48 +0700

    init

---
+++ imagemin.js
@@ -0,0 +1,68 @@
+const imagemin = require('imagemin');
+const imageminJpegRecompress = require('imagemin-jpeg-recompress');
+//requiring path and fs modules
+const path = require('path');
+const fs = require('fs');
+//joining path of directory
+const directoryPath = path.join(__dirname, 'images');
+// const directoryPath = '/Users/doducnhat/Documents/Pictures/100D3400';
+// passsing directoryPath and callback function
+fs.readdir(directoryPath, async function (err, files) {
+	//handling error
+	if (err) {
+		return console.log('Unable to scan directory: ' + err);
+	}
+	let count = 0;
+	let totalTime = 0;
+	let max = 0;
+	let min = 0;
+	//listing all files using forEach
+	for (let filename of files) {
+		// Do whatever you want to do with the file
+		if (/^.*\.(jpe?g|JPE?G)$/.test(filename)) {
+			// const file = path.join(__dirname, `images/${filename}`);
+			const file = path.join(directoryPath, filename);
+			console.log(file);
+
+			let time = await compressImage(file, filename);
+			totalTime += time
+
+			if (min < 1) {
+				min = time
+			} else {
+				if (time < min) {
+					min = time
+				}
+			}
+
+			if (max < 1) {
+				max = time
+			} else {
+				if (time > max) {
+					max = time
+				}
+			}
+
+			count++;
+		}
+	}
+
+	console.log(`Compressed ${count} images in ${totalTime}. AVG: ${totalTime / count}. MIN: ${min}. MAX: ${max}`);
+});
+
+async function compressImage(file, filename) {
+	const start = new Date();
+	await imagemin([
+		file
+	], {
+		destination: 'results/imagemin',
+		plugins: [
+			imageminJpegRecompress()
+		]
+	});
+	const end = new Date() - start;
+
+	console.log(`Images optimized: ${filename} - Time: ${end}`);
+
+	return end;
+}
---
+++ readme.md
@@ -0,0 +1,11 @@
+# Jpeg Recompress Benchmark Script
+
+This script was created to benchmark imagemin jpeg-recompress plugin vs jpeg-recompress binary
+
+## How to run
+
+### Binary
+`npm run binary`
+
+### Imagemin
+`npm run imagemin`
